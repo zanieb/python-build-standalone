ef4bab96d35057c381bf56896fda45f8f3d341cb
Zanie Blue <contact@zanie.dev>
2024-09-26 07:55:28 -0500

    Normalize musl triple in xextproto build

--- cpython-unix/build-xextproto.sh
+++ cpython-unix/build-xextproto.sh
@@ -15,9 +15,10 @@ export PKG_CONFIG_PATH=/tools/deps/share/pkgconfig
 tar -xf xextproto-${XEXTPROTO_VERSION}.tar.gz
 pushd xextproto-${XEXTPROTO_VERSION}
 
+# xextproto does not support musl targets so we pretend it is gnu
 CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CPPFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" LDFLAGS="${EXTRA_TARGET_LDFLAGS}" ./configure \
     --build=${BUILD_TRIPLE} \
-    --host=${TARGET_TRIPLE} \
+    --host="${TARGET_TRIPLE/-musl/-gnu}" \
     --prefix=/tools/deps
 
 make -j `nproc`
