a55a70233043d00f27039fb6de68dd1087713a40
Zanie Blue <contact@zanie.dev>
2024-09-25 21:58:28 -0500

    Disable runtime checks during expat build

--- cpython-unix/build-expat.sh
+++ cpython-unix/build-expat.sh
@@ -24,7 +24,20 @@ CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CPPFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" ./
     --without-examples \
     --without-tests \
     --without-xmlwf \
-    ax_cv_check_cflags___fexceptions=no
+    ax_cv_check_cflags___fexceptions=no \
+    ac_cv_prog_cc_works=yes \
+    ac_cv_prog_cxx_works=yes \
+    ac_cv_func_malloc_0_nonnull=yes \
+    ac_cv_func_memset=yes \
+    ac_cv_func_calloc=yes \
+    ac_cv_func_realloc=yes \
+    ac_cv_func_memcpy=yes \
+    ac_cv_func_strchr=yes \
+    ac_cv_func_strrchr=yes \
+    ac_cv_func_strcmp=yes \
+    ac_cv_func_strncpy=yes \
+    ac_cv_func_strlen=yes \
+    --disable-option-checking
 
 make -j ${NUM_CPUS}
 make -j ${NUM_CPUS} install DESTDIR=${ROOT}/out
